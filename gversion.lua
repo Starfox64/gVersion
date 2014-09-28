146a78cce0537d4ea9426f36f2b58cfd1f3ca0af
_FR_Starfox64 <louisdijon21@yahoo.fr>
2014-09-28 18:02:08 +0200

    Conflicts Update
    Security update to fix possible conflicts when multiple addons using
    gVersion are running.

--- gversion.lua
+++ gversion.lua
@@ -1,7 +1,7 @@
 local gVersion = {}
 gVersion.FilePath = "data/gversion_version.txt" -- Where is the local version file stored.
-gVersion.Cloud = "https://raw.githubusercontent.com/Starfox64/gVersion/master/data/gversion_version.txt" -- The URL of the remote version file if you are using a file from github you can get this link by hitting 'RAW' on it.
-gVersion.AddonName = "gVersion" -- The name of your addon.
+gVersion.Cloud = "https://raw.githubusercontent.com/Starfox64/gVersion/master/data/gversion_version.txt" -- The URL of the remote version file if you are using a file from GitHub you can get this link by hitting 'RAW' on it.
+gVersion.AddonName = "gVersion" -- The name of your addon. (Must be unique to avoid conflicts with other gVersion instances.)
 gVersion.NotifyClient = true -- Should gVersion echo whether or not the addon is up to date to clients?
 
 if SERVER then
@@ -16,13 +16,12 @@ if SERVER then
 					MsgC(Color(255, 100, 100), gVersion.AddonName.." could not find it's local version!\n")
 				end
 				gVersion.Remote = tonumber(data) or 0
+				MsgC(Color(255, 100, 100), "[gVersion] ")
 				if gVersion.Remote <= gVersion.Local then
 					gVersion.Status = 3
-					MsgC(Color(255, 100, 100), "[gVersion] ")
 					MsgC(Color(100, 255, 100), gVersion.AddonName.." is up to date! (Revision: "..gVersion.Remote..")\n")
 				else
 					gVersion.Status = 2
-					MsgC(Color(255, 100, 100), "[gVersion] ")
 					MsgC(Color(255, 255, 100), gVersion.AddonName.." is not up to date! (Local: "..gVersion.Local..", Remote: "..gVersion.Remote..")\n")
 				end
 			end,
@@ -35,27 +34,31 @@ if SERVER then
 		)
 	end
 	gVersion.CheckForUpdates()
+end
 
-	if gVersion.NotifyClient then
+if gVersion.NotifyClient then
+	if SERVER then
 		util.AddNetworkString("gVersionNotify")
-		hook.Add("PlayerInitialSpawn", "gVersion"..gVersion.AddonName, function( ply )
+		hook.Add("PlayerInitialSpawn", "gVersion_"..gVersion.AddonName, function( ply )
 			net.Start("gVersionNotify")
 			net.WriteInt(gVersion.Status or 0, 4)
+			net.WriteString(gVersion.AddonName)
 			net.Send(ply)
 		end)
+	else
+		net.Receive("gVersionNotify", function()
+			local status = net.ReadInt(4)
+			local name = net.ReadString()
+			MsgC(Color(255, 100, 100), "[gVersion] ")
+			if status == 0 then
+				MsgC(Color(255, 100, 100), name.." unknown error!\n")
+			elseif status == 1 then
+				MsgC(Color(255, 100, 100), name.." could not check for updates!\n")
+			elseif status == 2 then
+				MsgC(Color(255, 255, 100), name.." is not up to date!\n")
+			elseif status == 3 then
+				MsgC(Color(100, 255, 100), name.." is up to date!\n")
+			end
+		end)
 	end
-else
-	net.Receive("gVersionNotify", function()
-		local status = net.ReadInt(4)
-		MsgC(Color(255, 100, 100), "[gVersion] ")
-		if status == 0 then
-			MsgC(Color(255, 100, 100), gVersion.AddonName.." unknown error!\n")
-		elseif status == 1 then
-			MsgC(Color(255, 100, 100), gVersion.AddonName.." could not check for updates!\n")
-		elseif status == 2 then
-			MsgC(Color(255, 255, 100), gVersion.AddonName.." is not up to date!\n")
-		elseif status == 3 then
-			MsgC(Color(100, 255, 100), gVersion.AddonName.." is up to date!\n")
-		end
-	end)
 end
